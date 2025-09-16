a99690def0fb2d779eeb890507d5c844dde16b42
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-09-16 09:22:21 +0200

    init

---
+++ src/game2025/game2025/App.java
@@ -0,0 +1,12 @@
+package game2025.game2025;
+
+import javafx.application.Application;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+			Application.launch(GUI.class);	
+
+	}
+}
